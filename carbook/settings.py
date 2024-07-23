601316a7004a4fdf7c7c4082f9817d8decbc5f76
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-07-23 17:22:09 +0000

    changws

--- carbook/settings.py
+++ carbook/settings.py
@@ -169,10 +169,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email configuration setup
 
-EMAIL_BACKEND = 'rental.backends.email_backend.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = 'False'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
